d3b67417b9d90a61562c14261c58f49f3df4c9a7
mcflumpkinsgames <margaret.k.mcnulty+gamesgit@gmail.com>
2024-02-27 21:18:51 -0800

    particle effects and incomplete path enemy

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var timer = $Timer
+@onready var path_enemy_timer = $PathEnemyTimer
 @onready var spawn_positions = $SpawnPositions
 
 signal enemy_spawned(enemy_instance)
@@ -9,9 +10,13 @@ var enemy = preload("res://scenes/enemy.tscn")
 
 func _ready():
 	timer.connect("timeout", _on_timeout)
+	path_enemy_timer.connect("timeout", _on_path_enemy_timeout)
 	
 func _on_timeout():
 	spawn_enemy()
+	
+func _on_path_enemy_timeout():
+	pass
 	
 func spawn_enemy():
 	var spawn_positions_array = spawn_positions.get_children()
